813b4ec9e03f68fc9eed8d7da15ddb5b9a451504
BpHana <57785153+BpHana@users.noreply.github.com>
2020-11-07 15:44:42 +0700

    NewProject

--- COS_DesktopApp/Properties/Resources.Designer.cs
+++ COS_DesktopApp/Properties/Resources.Designer.cs
@@ -63,9 +63,9 @@ namespace COS_DesktopApp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap Screenshot_2020_11_07_143737 {
+        internal static System.Drawing.Bitmap chef128 {
             get {
-                object obj = ResourceManager.GetObject("Screenshot 2020-11-07 143737", resourceCulture);
+                object obj = ResourceManager.GetObject("chef128", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
