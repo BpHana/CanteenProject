bd9c9e18939770bbed0f9ea02fd2e7a2943f6191
BpHana <57785153+BpHana@users.noreply.github.com>
2020-11-07 20:15:18 +0700

    Update Program.cs

--- COS_DesktopApp/Program.cs
+++ COS_DesktopApp/Program.cs
@@ -19,6 +19,7 @@ namespace COS_DesktopApp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginForm());
+            Console.WriteLine("abc");
         }
     }
 }
