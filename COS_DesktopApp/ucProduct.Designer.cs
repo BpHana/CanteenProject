fe9b9c4b96bca931c064c724d313c130ae4079e0
Thuan0707 <thuandmce140131@fpt.edu.vn>
2020-11-09 22:57:03 +0700

    a

--- COS_DesktopApp/ucProduct.Designer.cs
+++ COS_DesktopApp/ucProduct.Designer.cs
@@ -29,42 +29,66 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            DevExpress.XtraBars.Docking2010.WindowsUIButtonImageOptions windowsUIButtonImageOptions1 = new DevExpress.XtraBars.Docking2010.WindowsUIButtonImageOptions();
+            DevExpress.XtraBars.Docking2010.WindowsUIButtonImageOptions windowsUIButtonImageOptions2 = new DevExpress.XtraBars.Docking2010.WindowsUIButtonImageOptions();
+            DevExpress.XtraGrid.GridLevelNode gridLevelNode1 = new DevExpress.XtraGrid.GridLevelNode();
             this.behaviorManager1 = new DevExpress.Utils.Behaviors.BehaviorManager(this.components);
+            this.btnDelete = new DevExpress.XtraBars.Docking2010.WindowsUIButtonPanel();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
             this.productsBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.productsBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
             this.colid = new DevExpress.XtraGrid.Columns.GridColumn();
             this.colname = new DevExpress.XtraGrid.Columns.GridColumn();
             this.colprice = new DevExpress.XtraGrid.Columns.GridColumn();
             this.colvolume = new DevExpress.XtraGrid.Columns.GridColumn();
             this.colingredient = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.colproducts_type = new DevExpress.XtraGrid.Columns.GridColumn();
             this.colcompany = new DevExpress.XtraGrid.Columns.GridColumn();
             this.colorigin = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colproducts_type = new DevExpress.XtraGrid.Columns.GridColumn();
             this.colsugar_level = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.colid_productsType = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.colid_origin = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.colid_sugarLevel = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.colid_company = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.coldeletedAt = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.colcarts = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.colorder_details = new DevExpress.XtraGrid.Columns.GridColumn();
             this.colimage = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.deletedAt = new DevExpress.XtraGrid.Columns.GridColumn();
             ((System.ComponentModel.ISupportInitialize)(this.behaviorManager1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            windowsUIButtonImageOptions1.Image = global::COS_DesktopApp.Properties.Resources.add;
+            windowsUIButtonImageOptions2.Image = global::COS_DesktopApp.Properties.Resources.trash;
+            this.btnDelete.Buttons.AddRange(new DevExpress.XtraEditors.ButtonPanel.IBaseButton[] {
+            new DevExpress.XtraBars.Docking2010.WindowsUIButton("Add", true, windowsUIButtonImageOptions1, DevExpress.XtraBars.Docking2010.ButtonStyle.PushButton, "", -1, true, null, true, false, true, null, -1, false),
+            new DevExpress.XtraBars.Docking2010.WindowsUIButton("Delete", true, windowsUIButtonImageOptions2, DevExpress.XtraBars.Docking2010.ButtonStyle.PushButton, "", -1, true, null, true, false, true, null, -1, false)});
+            this.btnDelete.ContentAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDelete.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDelete.Location = new System.Drawing.Point(0, 0);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Padding = new System.Windows.Forms.Padding(25, 0, 0, 0);
+            this.btnDelete.Size = new System.Drawing.Size(700, 87);
+            this.btnDelete.TabIndex = 0;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.DataSource = this.productsBindingSource;
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            gridLevelNode1.RelationName = "Level1";
+            this.gridControl1.LevelTree.Nodes.AddRange(new DevExpress.XtraGrid.GridLevelNode[] {
+            gridLevelNode1});
+            this.gridControl1.Location = new System.Drawing.Point(0, 87);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(700, 535);
+            this.gridControl1.TabIndex = 1;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
             // 
             // productsBindingSource
             // 
             this.productsBindingSource.DataSource = typeof(COS_DesktopApp.product);
-            // 
-            // productsBindingSource1
-            // 
-            this.productsBindingSource1.DataSource = typeof(COS_DesktopApp.product);
             // 
             // gridView1
             // 
@@ -74,22 +98,17 @@
             this.colprice,
             this.colvolume,
             this.colingredient,
-            this.colproducts_type,
             this.colcompany,
             this.colorigin,
+            this.colproducts_type,
             this.colsugar_level,
-            this.colid_productsType,
-            this.colid_origin,
-            this.colid_sugarLevel,
-            this.colid_company,
-            this.coldeletedAt,
-            this.colcarts,
-            this.colorder_details,
-            this.colimage});
+            this.colimage,
+            this.deletedAt});
             this.gridView1.GridControl = this.gridControl1;
             this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsFilter.AllowAutoFilterConditionChange = DevExpress.Utils.DefaultBoolean.True;
+            this.gridView1.OptionsFilter.AllowFilterEditor = false;
             this.gridView1.OptionsView.ShowFilterPanelMode = DevExpress.XtraGrid.Views.Base.ShowFilterPanelMode.Never;
-            this.gridView1.RowUpdated += new DevExpress.XtraGrid.Views.Base.RowObjectEventHandler(this.gridView1_RowUpdated);
             // 
             // colid
             // 
@@ -141,16 +160,6 @@
             this.colingredient.VisibleIndex = 5;
             this.colingredient.Width = 94;
             // 
-            // colproducts_type
-            // 
-            this.colproducts_type.Caption = "Type";
-            this.colproducts_type.FieldName = "products_type.name";
-            this.colproducts_type.MinWidth = 25;
-            this.colproducts_type.Name = "colproducts_type";
-            this.colproducts_type.Visible = true;
-            this.colproducts_type.VisibleIndex = 3;
-            this.colproducts_type.Width = 94;
-            // 
             // colcompany
             // 
             this.colcompany.Caption = "Company";
@@ -171,67 +180,25 @@
             this.colorigin.VisibleIndex = 7;
             this.colorigin.Width = 94;
             // 
+            // colproducts_type
+            // 
+            this.colproducts_type.Caption = "Type";
+            this.colproducts_type.FieldName = "products_type.name";
+            this.colproducts_type.MinWidth = 25;
+            this.colproducts_type.Name = "colproducts_type";
+            this.colproducts_type.Visible = true;
+            this.colproducts_type.VisibleIndex = 3;
+            this.colproducts_type.Width = 94;
+            // 
             // colsugar_level
             // 
-            this.colsugar_level.Caption = "Sugar Level";
+            this.colsugar_level.Caption = "Sugar";
             this.colsugar_level.FieldName = "sugar_level.name";
             this.colsugar_level.MinWidth = 25;
             this.colsugar_level.Name = "colsugar_level";
             this.colsugar_level.Visible = true;
             this.colsugar_level.VisibleIndex = 8;
             this.colsugar_level.Width = 94;
-            // 
-            // colid_productsType
-            // 
-            this.colid_productsType.Caption = "Type";
-            this.colid_productsType.FieldName = "products_type.name";
-            this.colid_productsType.MinWidth = 25;
-            this.colid_productsType.Name = "colid_productsType";
-            this.colid_productsType.Width = 94;
-            // 
-            // colid_origin
-            // 
-            this.colid_origin.Caption = "Origin";
-            this.colid_origin.FieldName = "origin.name";
-            this.colid_origin.MinWidth = 25;
-            this.colid_origin.Name = "colid_origin";
-            this.colid_origin.Width = 94;
-            // 
-            // colid_sugarLevel
-            // 
-            this.colid_sugarLevel.FieldName = "sugar_level.name";
-            this.colid_sugarLevel.MinWidth = 25;
-            this.colid_sugarLevel.Name = "colid_sugarLevel";
-            this.colid_sugarLevel.Width = 94;
-            // 
-            // colid_company
-            // 
-            this.colid_company.Caption = "Company";
-            this.colid_company.FieldName = "company.name";
-            this.colid_company.MinWidth = 25;
-            this.colid_company.Name = "colid_company";
-            this.colid_company.Width = 94;
-            // 
-            // coldeletedAt
-            // 
-            this.coldeletedAt.FieldName = "deletedAt";
-            this.coldeletedAt.MinWidth = 25;
-            this.coldeletedAt.Name = "coldeletedAt";
-            this.coldeletedAt.Width = 94;
-            // 
-            // colcarts
-            // 
-            this.colcarts.FieldName = "carts";
-            this.colcarts.MinWidth = 25;
-            this.colcarts.Name = "colcarts";
-            this.colcarts.Width = 94;
-            // 
-            // colorder_details
-            // 
-            this.colorder_details.FieldName = "order_details";
-            this.colorder_details.MinWidth = 25;
-            this.colorder_details.Name = "colorder_details";
-            this.colorder_details.Width = 94;
             // 
             // colimage
             // 
@@ -243,60 +210,47 @@
             this.colimage.VisibleIndex = 9;
             this.colimage.Width = 94;
             // 
-            // gridControl1
+            // deletedAt
             // 
-            this.gridControl1.DataSource = this.productsBindingSource1;
-            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.gridControl1.Location = new System.Drawing.Point(0, 0);
-            this.gridControl1.LookAndFeel.SkinName = "Office 2019 Colorful";
-            this.gridControl1.LookAndFeel.Style = DevExpress.LookAndFeel.LookAndFeelStyle.Style3D;
-            this.gridControl1.MainView = this.gridView1;
-            this.gridControl1.Name = "gridControl1";
-            this.gridControl1.Size = new System.Drawing.Size(556, 312);
-            this.gridControl1.TabIndex = 0;
-            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
-            this.gridView1});
+            this.deletedAt.Caption = "deleteAt";
+            this.deletedAt.FieldName = "deletedAt";
+            this.deletedAt.MinWidth = 25;
+            this.deletedAt.Name = "deletedAt";
+            this.deletedAt.Width = 94;
             // 
             // ucProduct
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.btnDelete);
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "ucProduct";
-            this.Size = new System.Drawing.Size(556, 622);
-            this.Load += new System.EventHandler(this.ucProduct_Load);
+            this.Size = new System.Drawing.Size(700, 622);
             ((System.ComponentModel.ISupportInitialize)(this.behaviorManager1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
         private DevExpress.Utils.Behaviors.BehaviorManager behaviorManager1;
+        private DevExpress.XtraBars.Docking2010.WindowsUIButtonPanel btnDelete;
+        private DevExpress.XtraGrid.GridControl gridControl1;
         private System.Windows.Forms.BindingSource productsBindingSource;
-        private System.Windows.Forms.BindingSource productsBindingSource1;
         private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
         private DevExpress.XtraGrid.Columns.GridColumn colid;
         private DevExpress.XtraGrid.Columns.GridColumn colname;
         private DevExpress.XtraGrid.Columns.GridColumn colprice;
         private DevExpress.XtraGrid.Columns.GridColumn colvolume;
         private DevExpress.XtraGrid.Columns.GridColumn colingredient;
-        private DevExpress.XtraGrid.Columns.GridColumn colproducts_type;
+        private DevExpress.XtraGrid.Columns.GridColumn colimage;
         private DevExpress.XtraGrid.Columns.GridColumn colcompany;
         private DevExpress.XtraGrid.Columns.GridColumn colorigin;
+        private DevExpress.XtraGrid.Columns.GridColumn colproducts_type;
         private DevExpress.XtraGrid.Columns.GridColumn colsugar_level;
-        private DevExpress.XtraGrid.Columns.GridColumn colid_productsType;
-        private DevExpress.XtraGrid.Columns.GridColumn colid_origin;
-        private DevExpress.XtraGrid.Columns.GridColumn colid_sugarLevel;
-        private DevExpress.XtraGrid.Columns.GridColumn colid_company;
-        private DevExpress.XtraGrid.Columns.GridColumn coldeletedAt;
-        private DevExpress.XtraGrid.Columns.GridColumn colcarts;
-        private DevExpress.XtraGrid.Columns.GridColumn colorder_details;
-        private DevExpress.XtraGrid.Columns.GridColumn colimage;
-        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Columns.GridColumn deletedAt;
     }
 }
