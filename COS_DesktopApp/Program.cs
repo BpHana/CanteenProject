b016d0b2e2437b04058199dbe5743e5e06f97e3e
Thuan0707 <thuandmce140131@fpt.edu.vn>
2020-11-09 14:29:48 +0700

    fb

--- COS_DesktopApp/Program.cs
+++ COS_DesktopApp/Program.cs
@@ -21,7 +21,7 @@ namespace COS_DesktopApp
     
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            Application.Run(new ManagementForm());
         }
     }
 }
