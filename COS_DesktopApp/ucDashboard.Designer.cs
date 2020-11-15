f8611ce8a1c36fb74b7c867839bb34d37f0e60ac
BpHana <57785153+BpHana@users.noreply.github.com>
2020-11-15 20:46:00 +0700

    .

--- COS_DesktopApp/ucDashboard.Designer.cs
+++ COS_DesktopApp/ucDashboard.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ucDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "ucDashboard";
+            this.Size = new System.Drawing.Size(620, 492);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
