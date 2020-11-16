8185cf5e0ade667a4a0c6839924895ad8ac60d40
TuanMinh <58020524+vutuanminh81@users.noreply.github.com>
2020-11-17 02:01:06 +0700

    Minh
    Change

---
+++ COS_WebApp/Controllers/CartController.cs
@@ -0,0 +1,109 @@
+﻿using COS_WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace COS_WebApp.Controllers
+{
+    public class CartController : Controller
+    {
+        CanteenOrderingSystemEntities cos = new CanteenOrderingSystemEntities();
+        // GET: Cart
+        public ActionResult Cart()
+        {
+            return View();
+        }
+
+        
+
+        public ActionResult AddToCart(int Id)
+        {
+            product prod = cos.products.Where(x => x.id == Id).SingleOrDefault();
+            
+           
+            return  View(prod);
+
+        }
+
+        List<ShoppingCart> listCart = new List<ShoppingCart>();
+        [HttpPost]
+        public ActionResult AddToCart(product prod, int Id)
+        {
+            return View();
+        }
+
+        // GET: Cart/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Cart/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cart/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cart/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Cart/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cart/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Cart/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
