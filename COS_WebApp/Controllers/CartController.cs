944dab89cf0526b670b26a118fe0dfccf11f7479
TuanMinh <58020524+vutuanminh81@users.noreply.github.com>
2020-11-18 14:28:23 +0700

    fds

--- COS_WebApp/Controllers/CartController.cs
+++ COS_WebApp/Controllers/CartController.cs
@@ -15,6 +15,7 @@ namespace COS_WebApp.Controllers
         // GET: Cart
         public ActionResult Cart()
         {
+
             int userid = (Session["User"] as account).id;
             var cart = cos.carts;
             foreach (var item in cart)
@@ -33,7 +34,7 @@ namespace COS_WebApp.Controllers
                     shopCart.Add(dto);
                 }
             }
-            
+
             return View(shopCart);
         }
 
@@ -43,32 +44,36 @@ namespace COS_WebApp.Controllers
             int userid = (Session["User"] as account).id;
 
             var query = from cart in cos.carts
-                        where cart.id_products ==idCart && cart.id_user==userid
+                        where cart.id_products == idCart && cart.id_user == userid
                         select cart;
-            if (query.SingleOrDefault()!=null)
+            if (query.SingleOrDefault() != null)
             {
                 cart obj = query.FirstOrDefault();
                 cos.carts.Remove(obj);
                 cos.SaveChanges();
             }
-           
+
 
             return RedirectToAction("Cart");
         }
         public ActionResult action()
         {
             return RedirectToAction("Cart");
-           
+
         }
 
         [HttpGet]
-        public ActionResult action(string[] id,string[] quantity,string btnAction)
+        public ActionResult action(string[] id, string[] quantity, string btnAction)
         {
             switch (btnAction)
             {
                 case "Update Cart":
                     updateCart(id, quantity);
+
                     break;
+                case "Order":
+                    return RedirectToAction("Order");
+
             }
             return RedirectToAction("Cart");
         }
@@ -76,8 +81,8 @@ namespace COS_WebApp.Controllers
         [HttpGet]
         public ActionResult updateCart(string[] id, string[] quantity)
         {
-    
-           
+
+
             int userid = (Session["User"] as account).id;
             for (int i = 0; i < id.Length; i++)
             {
@@ -86,12 +91,12 @@ namespace COS_WebApp.Controllers
                 var query = from cart in cos.carts
                             where cart.id_products == idCart && cart.id_user == userid
                             select cart;
-             
-                    cart obj = query.FirstOrDefault();
+
+                cart obj = query.FirstOrDefault();
                 obj.quantity = quantityP;
-                   
-                    cos.SaveChanges();
-               
+
+                cos.SaveChanges();
+
 
             }
 
@@ -121,15 +126,79 @@ namespace COS_WebApp.Controllers
             cos.SaveChanges();
 
 
-            return  View("Cart");
+            return View("Cart");
 
         }
 
         public ActionResult Order()
         {
-            int accountid = (Session["User"] as account).id;
+            int userid = (Session["User"] as account).id;
+            var cart = cos.carts;
+            foreach (var item in cart)
+            {
+                if (item.id_user == userid)
+                {
+                    var query = from product in cos.products
+                                where product.id == item.id_products
+                                select product;
+                    product prod = query.FirstOrDefault();
+                    ShoppingCart dto = new ShoppingCart(prod, item.quantity);
+                    if (shopCart == null)
+                    {
+                        shopCart = new List<ShoppingCart>();
+                    }
+                    shopCart.Add(dto);
+                }
+            }
 
-            return View();
+
+            return View(shopCart);
+        }
+
+        public ActionResult AddOrder(String address)
+        {
+            int userid = (Session["User"] as account).id;
+            decimal totalprice = 0;
+            var cart = cos.carts;
+            foreach (var item in cart)
+            {
+                if (item.id_user == userid)
+                {
+                    var query = from product in cos.products
+                                where product.id == item.id_products
+                                select product;
+                    product prod = query.FirstOrDefault();
+                    totalprice += prod.price * item.quantity;
+                    ShoppingCart dto = new ShoppingCart(prod, item.quantity);
+                    if (shopCart == null)
+                    {
+                        shopCart = new List<ShoppingCart>();
+                    }
+                    shopCart.Add(dto);
+                }
+            }
+
+            order od = new order { id_user = userid, totalprice = totalprice, createdAt = DateTime.Now, address = address };
+            cos.orders.Add(od);
+            cos.SaveChanges();
+
+            var query1 = from order in cos.orders
+                         where order.id_user == userid
+                         select order;
+
+            query1 = query1.OrderByDescending(o => o.id);
+            order or = query1.FirstOrDefault();
+            foreach (var item in shopCart)
+            {
+                order_details detail = new order_details { id_order = or.id, id_products = item.Product.id, quantity = item.Quantity, createdAt = or.createdAt };
+                cos.order_details.Add(detail);
+               
+
+                cos.carts.RemoveRange(cos.carts.Where(x => x.id_user == userid));
+                cos.SaveChanges();
+            }
+            return RedirectToAction("Index", "Home");
+
         }
 
 
