d455887d73697d2d763f6d46470cd9ed5e3d7f7e
BpHana <57785153+BpHana@users.noreply.github.com>
2020-11-18 14:50:03 +0700

    Update ucOrder.Designer.cs

--- COS_DesktopApp/ucOrder.Designer.cs
+++ COS_DesktopApp/ucOrder.Designer.cs
@@ -43,6 +43,8 @@
             this.colorder = new DevExpress.XtraGrid.Columns.GridColumn();
             this.colproduct = new DevExpress.XtraGrid.Columns.GridColumn();
             this.colUser = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colImage = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.repositoryItemPictureEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit();
             this.repositoryItemGridLookUpEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemGridLookUpEdit();
             this.repositoryItemGridLookUpEdit1View = new DevExpress.XtraGrid.Views.Grid.GridView();
             this.ordersBindingSource = new System.Windows.Forms.BindingSource(this.components);
@@ -51,6 +53,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.order_detailsBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemPictureEdit1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.repositoryItemGridLookUpEdit1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.repositoryItemGridLookUpEdit1View)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ordersBindingSource)).BeginInit();
@@ -62,12 +65,15 @@
             // 
             this.gridControl1.DataSource = this.order_detailsBindingSource;
             this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.gridControl1.Location = new System.Drawing.Point(0, 0);
             this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.gridControl1.Name = "gridControl1";
             this.gridControl1.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
-            this.repositoryItemGridLookUpEdit1});
-            this.gridControl1.Size = new System.Drawing.Size(626, 494);
+            this.repositoryItemGridLookUpEdit1,
+            this.repositoryItemPictureEdit1});
+            this.gridControl1.Size = new System.Drawing.Size(730, 608);
             this.gridControl1.TabIndex = 1;
             this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
@@ -89,12 +95,15 @@
             this.coldeletedAt,
             this.colorder,
             this.colproduct,
-            this.colUser});
+            this.colUser,
+            this.colImage});
+            this.gridView1.DetailHeight = 431;
             this.gridView1.GridControl = this.gridControl1;
             this.gridView1.GroupCount = 1;
             this.gridView1.Name = "gridView1";
             this.gridView1.OptionsBehavior.Editable = false;
             this.gridView1.OptionsCustomization.AllowGroup = false;
+            this.gridView1.RowHeight = 123;
             this.gridView1.SortInfo.AddRange(new DevExpress.XtraGrid.Columns.GridColumnSortInfo[] {
             new DevExpress.XtraGrid.Columns.GridMergedColumnSortInfo(new DevExpress.XtraGrid.Columns.GridColumn[] {
                         this.colid_order,
@@ -110,52 +119,64 @@
             // 
             this.colTotalPrice.Caption = "Total Price";
             this.colTotalPrice.FieldName = "order.totalprice";
+            this.colTotalPrice.MinWidth = 23;
             this.colTotalPrice.Name = "colTotalPrice";
             this.colTotalPrice.OptionsColumn.AllowMove = false;
             this.colTotalPrice.Visible = true;
             this.colTotalPrice.VisibleIndex = 0;
+            this.colTotalPrice.Width = 87;
             // 
             // colid_order
             // 
             this.colid_order.Caption = "ID Order";
             this.colid_order.FieldName = "id_order";
+            this.colid_order.MinWidth = 23;
             this.colid_order.Name = "colid_order";
             this.colid_order.OptionsColumn.AllowMove = false;
             this.colid_order.Visible = true;
             this.colid_order.VisibleIndex = 0;
+            this.colid_order.Width = 87;
             // 
             // colid_products
             // 
             this.colid_products.Caption = "Product";
             this.colid_products.FieldName = "product.name";
+            this.colid_products.MinWidth = 23;
             this.colid_products.Name = "colid_products";
             this.colid_products.Visible = true;
             this.colid_products.VisibleIndex = 0;
+            this.colid_products.Width = 87;
             // 
             // colAddress
             // 
             this.colAddress.Caption = "Address";
             this.colAddress.FieldName = "order.address";
+            this.colAddress.MinWidth = 23;
             this.colAddress.Name = "colAddress";
             this.colAddress.OptionsColumn.AllowMove = false;
             this.colAddress.Visible = true;
             this.colAddress.VisibleIndex = 0;
+            this.colAddress.Width = 87;
             // 
             // colquantity
             // 
             this.colquantity.Caption = "Quantity";
             this.colquantity.FieldName = "quantity";
+            this.colquantity.MinWidth = 23;
             this.colquantity.Name = "colquantity";
             this.colquantity.Visible = true;
-            this.colquantity.VisibleIndex = 1;
+            this.colquantity.VisibleIndex = 2;
+            this.colquantity.Width = 87;
             // 
             // colprice
             // 
             this.colprice.Caption = "Price";
             this.colprice.FieldName = "price";
+            this.colprice.MinWidth = 23;
             this.colprice.Name = "colprice";
             this.colprice.Visible = true;
-            this.colprice.VisibleIndex = 2;
+            this.colprice.VisibleIndex = 3;
+            this.colprice.Width = 87;
             // 
             // colcreatedAt
             // 
@@ -163,33 +184,58 @@
             this.colcreatedAt.DisplayFormat.FormatString = "dd/MM/yyyy";
             this.colcreatedAt.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime;
             this.colcreatedAt.FieldName = "order.createdAt";
+            this.colcreatedAt.MinWidth = 23;
             this.colcreatedAt.Name = "colcreatedAt";
             this.colcreatedAt.Visible = true;
-            this.colcreatedAt.VisibleIndex = 3;
+            this.colcreatedAt.VisibleIndex = 4;
+            this.colcreatedAt.Width = 87;
             // 
             // coldeletedAt
             // 
             this.coldeletedAt.FieldName = "deletedAt";
+            this.coldeletedAt.MinWidth = 23;
             this.coldeletedAt.Name = "coldeletedAt";
+            this.coldeletedAt.Width = 87;
             // 
             // colorder
             // 
             this.colorder.FieldName = "order";
+            this.colorder.MinWidth = 23;
             this.colorder.Name = "colorder";
+            this.colorder.Width = 87;
             // 
             // colproduct
             // 
             this.colproduct.FieldName = "product";
+            this.colproduct.MinWidth = 23;
             this.colproduct.Name = "colproduct";
+            this.colproduct.Width = 87;
             // 
             // colUser
             // 
             this.colUser.Caption = "User Fullname";
             this.colUser.FieldName = "order.account.fullname";
+            this.colUser.MinWidth = 23;
             this.colUser.Name = "colUser";
             this.colUser.OptionsColumn.AllowMove = false;
             this.colUser.Visible = true;
             this.colUser.VisibleIndex = 0;
+            this.colUser.Width = 87;
+            // 
+            // colImage
+            // 
+            this.colImage.Caption = "Product Image";
+            this.colImage.ColumnEdit = this.repositoryItemPictureEdit1;
+            this.colImage.FieldName = "product.image";
+            this.colImage.Name = "colImage";
+            this.colImage.Visible = true;
+            this.colImage.VisibleIndex = 1;
+            this.colImage.Width = 87;
+            // 
+            // repositoryItemPictureEdit1
+            // 
+            this.repositoryItemPictureEdit1.Name = "repositoryItemPictureEdit1";
+            this.repositoryItemPictureEdit1.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Zoom;
             // 
             // repositoryItemGridLookUpEdit1
             // 
@@ -201,6 +247,7 @@
             // 
             // repositoryItemGridLookUpEdit1View
             // 
+            this.repositoryItemGridLookUpEdit1View.DetailHeight = 431;
             this.repositoryItemGridLookUpEdit1View.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus;
             this.repositoryItemGridLookUpEdit1View.Name = "repositoryItemGridLookUpEdit1View";
             this.repositoryItemGridLookUpEdit1View.OptionsSelection.EnableAppearanceFocusedCell = false;
@@ -220,14 +267,16 @@
             // 
             // ucOrder
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.gridControl1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "ucOrder";
-            this.Size = new System.Drawing.Size(626, 494);
+            this.Size = new System.Drawing.Size(730, 608);
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.order_detailsBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemPictureEdit1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.repositoryItemGridLookUpEdit1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.repositoryItemGridLookUpEdit1View)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ordersBindingSource)).EndInit();
@@ -257,5 +306,7 @@
         private System.Windows.Forms.BindingSource accountsBindingSource;
         private System.Windows.Forms.BindingSource productsBindingSource;
         private DevExpress.XtraGrid.Columns.GridColumn colTotalPrice;
+        private DevExpress.XtraGrid.Columns.GridColumn colImage;
+        private DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit repositoryItemPictureEdit1;
     }
 }
